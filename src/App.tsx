0bb98eb49a2012a7ebc4db8a4711f063a1780fa9
buddy5230 <74503111+buddy5230@users.noreply.github.com>
2023-06-16 22:46:39 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App: React.FC = () => {
-  const [inputNumbers, setInputNumbers] = useState<string>(""); 
+  const [inputNumbers, setInputNumbers] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
   const [result, setResult] = useState<string | null>(null);
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setInputNumbers(event.target.value);
   };
 
-  const game24 = (nums: number[]): string => {
+  const game24 = (nums: number[]) => {
     const target = 24;
     const operations = ["+", "-", "*", "/"];
 
@@ -25,30 +26,37 @@ const App: React.FC = () => {
         }
       }
     }
-
-    return "ไม่สามารถสร้างตัวเลข 24 ได้"; //กรณีทำให้เป็น 24 ไม่ได้
+    setErrorMessage("ไม่สามารถสร้างตัวเลข 24 ได้");
+    return null; //กรณีทำให้เป็น 24 ไม่ได้
   };
   const calculateResult = () => {
-    //console.log(inputNumbers)
-    const numbers = inputNumbers.split(",").map((num) => num.trim());;
-    console.log(numbers)
+    setErrorMessage("");
+    setResult("");
+    const numbers = inputNumbers.split(",").map((num) => num.trim());
+    console.log(numbers);
     for (let i = 0; i < numbers.length; i++) {
-      if (isNaN(Number(numbers[i]))) { //กรณีใส่ string อื่นที่ไม่ใช่ตัวเลข
-        return setResult("กรุณาใส่ตัวเลข 1-9 เท่านั้น");
-      }else if(numbers[i]===''){ //กรณีใส่ , เเล้วไม่เติมเลข หรือจัดformatผิด
-        return setResult("กรุณาใส่ตัวเลข 1-9 หรือใส่เครื่องหมาย , ให้ถูกต้อง");
+      if (isNaN(Number(numbers[i]))) {
+        //กรณีใส่ string อื่นที่ไม่ใช่ตัวเลข
+        return setErrorMessage("กรุณาใส่ตัวเลข 1-9 เท่านั้น");
+      } else if (numbers[i] === "") {
+        //กรณีใส่ , เเล้วไม่เติมเลข หรือจัดformatผิด
+        return setErrorMessage(
+          "กรุณาใส่ตัวเลข 1-9 หรือใส่เครื่องหมาย , ให้ถูกต้อง"
+        );
       }
     }
-    
+
     const numbers1 = numbers.map((num) => parseFloat(num));
-    console.log(numbers1)
-    if (numbers.length !== 4) { //กรณีใส่เลขมากกว่าหรือน้อยกว่า4ตัว
-      return setResult("กรุณาใส่ตัวเลขให้ครบ 4 ตัว");
+    console.log(numbers1);
+    if (numbers.length !== 4) {
+      //กรณีใส่เลขมากกว่าหรือน้อยกว่า4ตัว
+      return setErrorMessage("กรุณาใส่ตัวเลขให้ครบ 4 ตัว");
     }
 
     for (const i of numbers1) {
-      if (i > 9) {//กรณีใส่เลขที่เกิน 9
-        return setResult("กรุณาใส่ตัวเลข 1-9");
+      if (i > 9) {
+        //กรณีใส่เลขที่เกิน 9
+        return setErrorMessage("กรุณาใส่ตัวเลข 1-9");
       }
     }
     setResult(game24(numbers1));
@@ -68,10 +76,11 @@ const App: React.FC = () => {
           ตรวจสอบ
         </button>
       </div>
+      <h2>ผลลัพธ์ :</h2>
+      <p className="error-message">{errorMessage}</p>
       <p className="result-message">{result}</p>
     </div>
   );
 };
 
 export default App;
-
